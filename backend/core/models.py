53c58789e533e1a86f5a1b0f37c15b37ab63b5fa
Dawid Zareba <dawid.zareba03@gmail.com>
2022-10-15 16:00:18 +0200

    Custom User model

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,10 +1,60 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
+
+from backend import settings
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, first_name, last_name, email, password=None, **extra_fields):
+        # Create and save new user
+        if not email:
+            raise ValueError('User must have an email address')
+        if not first_name:
+            raise ValueError('User must have a first name')
+        if not last_name:
+            raise ValueError('User must have a last name')
+
+        user = self.model(
+            first_name=first_name,
+            last_name=last_name,
+            email=self.normalize_email(email),
+            **extra_fields,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, first_name, last_name, email, password):
+        # Create new superuser
+        user = self.create_user(first_name, last_name, email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    # Custom user model
+
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+    USERNAME_FIELD = 'email'
 
 
 class Message(models.Model):
-    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender')
-    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiver')
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='receiver')
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
