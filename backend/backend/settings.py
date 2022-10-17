1009135b60bf0446ad169203e3dcb5aa9d25add4
Dawid Zaręba <68419282+Fejren@users.noreply.github.com>
2022-10-17 19:59:17 +0200

    Authentication (#1)
    * Serializers for user and auth token
    
    * Views and urls for user, token

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'core.apps.CoreConfig',
     'corsheaders',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import generics, authentication, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-# Create your views here.
+from .serializers import UserSerializer, AuthTokenSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    # Create new user in the db
+    serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    # Create new auth token for user
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
