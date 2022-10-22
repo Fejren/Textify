e61e32e4b1c3b2343dab6365a406381a30013c03
Dawid Zareba <dawid.zareba03@gmail.com>
2022-10-22 16:50:26 +0200

    Actions and reducers for login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,21 +7,26 @@ import {
   Route,
 } from "react-router-dom";
 
+import { Provider } from 'react-redux';
+import store from './store'
+
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 
 const App = () => (
-  <Router>
-    <Switch>
-      <Route exact path='/' component={Home} />
-      <Route exact path='/login' component={Login} />
-      <Route exact path='/register' component={Register} />
-      {/*<Route exact path='/reset-password' component={} />*/}
-      {/*<Route exact path='/password/reset/confirm/:uid/:token' component={} />*/}
-      {/*<Route exact path='/activate/:uid/:token' component={} />*/}
-    </Switch>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+        {/*<Route exact path='/reset-password' component={} />*/}
+        {/*<Route exact path='/password/reset/confirm/:uid/:token' component={} />*/}
+        {/*<Route exact path='/activate/:uid/:token' component={} />*/}
+      </Switch>
+    </Router>
+  </Provider>
 );
 
 export default App;
