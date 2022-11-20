064a48539bda5974d7319d2cd98eb5b2b4d8b293
Dawid Zareba <dawid.zareba03@gmail.com>
2022-11-20 20:05:37 +0100

    Fixed actions, reducers for message

---
+++ frontend/src/actions/message.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+import {
+  ADD_MESSAGE,
+  SET_MESSAGES,
+  GET_CHATS_SUCCESS
+} from "./types";
+
+import { API_URL } from "../settings";
+
+export const addMessage = message => async dispatch => {
+  dispatch ({
+    type: ADD_MESSAGE,
+    message: message
+  });
+};
+
+export const setMessages = messages => async dispatch => {
+  dispatch ({
+    type: SET_MESSAGES,
+    messages: messages
+  });
+};
+
+const getUserChatsSuccess = chats => async dispatch => {
+  dispatch ({
+    type: GET_CHATS_SUCCESS,
+    chats: chats
+  });
+};
+
+export const getUserChats = (userId, token) => {
+  return dispatch => {
+    axios.defaults.headers = {
+      "Content-Type": "application/json",
+      Authorization: `JWT ${token}`
+    };
+    axios
+      .get(`${API_URL}/chat/?id=${userId}`)
+      .then(res => dispatch(getUserChatsSuccess(res.data)));
+  };
+};
