4846832521548d9d91e4a2436a545ab582b002f7
Dawid Zareba <dawid.zareba03@gmail.com>
2022-11-27 11:36:56 +0100

    performance

--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -1,63 +1,57 @@
-import React, {Component} from "react";
-import { connect } from "react-redux";
+import React, {useState} from 'react';
+
 import WebSocketInstance from "../websocket";
+import {connect} from "react-redux";
 import {useParams} from "react-router-dom";
 
-function withParams(Component) {
-  return props => <Component {...props} params={useParams()} />
-}
+const Chat = ({ user, messages }) => {
+  let [message, setMessage] = useState("")
+  let [isChat, setIsChat] = useState(null)
+  let currentUser = {...user}
+  let { chatId } = useParams()
 
-class Chat extends Component {
-  state = { message: "" };
-  currentUser = {...this.props.user}
-  chatId = {...this.props.params}.chatId
-
-  initialiseChat() {
-    this.waitForSocketConnection(() => {
+  function initialiseChat() {
+    waitForSocketConnection(() => {
       WebSocketInstance.fetchMessages(
-        this.currentUser.id,
-        this.chatId
+        currentUser.id,
+        chatId
       );
     });
-    console.log(this.chatId);
-    WebSocketInstance.connect(this.chatId);
+    console.log(chatId)
+    WebSocketInstance.connect(chatId)
   }
 
-  constructor(props) {
-    super(props);
-    this.initialiseChat();
+  if (!isChat && chatId) {
+    setIsChat(1)
+    initialiseChat()
   }
 
-  waitForSocketConnection(callback) {
-    const component = this;
-    setTimeout(function() {
+
+  function waitForSocketConnection(callback) {
+    setTimeout(function () {
       if (WebSocketInstance.state() === 1) {
-        console.log("Connection is made");
+        console.log("Connection is made")
         callback();
         return;
       } else {
-        console.log("wait for connection...");
-        component.waitForSocketConnection(callback);
+        console.log("Waiting for connection...")
+        waitForSocketConnection(callback)
       }
-    }, 10000);
+    }, 1000)
   }
 
-  messageChangeHandler = event => {
-    this.setState({ message: event.target.value });
-  };
-
-  sendMessageHandler = e => {
-    e.preventDefault();
+  const sendMessageHandler = event => {
+    event.preventDefault();
     const messageObject = {
-      from_user: this.currentUser,
-      content: this.state.message,
-      chatId: this.chatId
+      from_user: currentUser,
+      content: message,
+      chatId: chatId
     };
-    WebSocketInstance.newChatMessage(messageObject);
-    this.setState({ message: "" });
-  };
+    WebSocketInstance.newChatMessage(messageObject)
+    setMessage("");
+  }
 
-  renderTimestamp = timestamp => {
+  const renderTimestamp = timestamp => {
     let prefix = "";
     const timeDiff = Math.round(
       (new Date().getTime() - new Date(timestamp).getTime()) / 60000
@@ -80,88 +74,54 @@ class Chat extends Component {
     return prefix;
   };
 
-  renderMessages = messages => {
-    const currentUser = `${this.currentUser.first_name} ${this.currentUser.last_name}`
+  const renderMessages = messages => {
     return messages.map((message, i, arr) => (
       <li
         key={message.id}
-        style={{ marginBottom: arr.length - 1 === i ? "300px" : "15px" }}
-        className={message.author === currentUser ? "sent" : "replies"}
+        style={{marginBottom: arr.length - 1 === i ? "300px" : "15px"}}
+        className={message.author === currentUser.id ? "sent" : "replies"}
       >
         <p>
           {message.content}
-          <br />
-          <small>{this.renderTimestamp(message.timestamp)}</small>
+          <br/>
+          <small>{renderTimestamp(message.timestamp)}</small>
         </p>
       </li>
     ));
   };
 
-  scrollToBottom = () => {
-    this.messagesEnd.scrollIntoView({ behavior: "smooth" });
-  };
-
-  componentDidMount() {
-    this.scrollToBottom();
-  }
-
-  componentDidUpdate() {
-    this.scrollToBottom();
-  }
-
-  // componentWillReceiveProps(newProps) {
-  //   if (this.state.chatId !== {...newProps.params}.chatId) {
-  //     WebSocketInstance.disconnect();
-  //     this.waitForSocketConnection(() => {
-  //       WebSocketInstance.fetchMessages(
-  //         this.currentUser.id,
-  //         {...newProps.params}.chatId
-  //       );
-  //     });
-  //     WebSocketInstance.connect({...newProps.params}.chatId);
-  //   }
-  // }
-
-
-  render() {
-    console.log(this.chatId)
-    return (
-      <>
-        <div className={'chat'}>
-          <div className={'chatInfo'}>
-          {/*  TODO: zrób Tobiasz Szpak / czas wiadomości / ma się wysyłać jedna wiadomość */}
-          </div>
-        <div className="messages">
-          <ul id="chat-log">
-            {this.props.messages && this.renderMessages(this.props.messages)}
-            <div
-              style={{ float: "left", clear: "both" }}
-              ref={el => {
-                this.messagesEnd = el;
-              }}
-            />
-          </ul>
-        </div>
-        <div className="messageInput">
-          <form onSubmit={this.sendMessageHandler}>
-              <input
-                onChange={this.messageChangeHandler}
-                value={this.state.message}
-                required
-                id="chat-message-input"
-                type="text"
-                placeholder="Napisz wiadomość..."
-              />
-              <i className="fa fa-paperclip attachment" aria-hidden="true" />
-              <button id="chat-message-submit" className="submit">Wyslij
-                <i className="fa fa-paper-plane" aria-hidden="true" />
-              </button>
-          </form>
-        </div>
-        </div>
-      </>
-    );
-  }
+  return (
+    <div className={'chat'}>
+      <div className={'chatInfo'}>
+        {/*  TODO: zrób Tobiasz Szpak / czas wiadomości / ma się wysyłać jedna wiadomość */}
+      </div>
+      <div className="messages">
+        <ul id="chat-log">
+          {messages && renderMessages(messages)}
+          <div
+            style={{float: "left", clear: "both"}}
+          />
+        </ul>
+      </div>
+      <div className="messageInput">
+        <form onSubmit={sendMessageHandler}>
+          {chatId && <input
+            onChange={(e)=>setMessage(e.target.value)}
+            value={message}
+            required
+            id="chat-message-input"
+            type="text"
+            placeholder="Napisz wiadomość..."
+          /> }
+          <i className="fa fa-paperclip attachment" aria-hidden="true"/>
+          <button id="chat-message-submit" className="submit">
+            Wyslij
+            <i className="fa fa-paper-plane" aria-hidden="true"/>
+          </button>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 const mapStateToProps = state => {
@@ -169,6 +129,6 @@ const mapStateToProps = state => {
     user: state.auth.user,
     messages: state.message.messages
   };
-};
+}
 
-export default connect(mapStateToProps)(withParams(Chat));
+export default connect(mapStateToProps)(Chat);
