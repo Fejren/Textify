8085288183ba8a86b2a26aa1950ba1b314f500fb
Dawid Zareba <dawid.zareba03@gmail.com>
2022-11-27 15:13:40 +0100

    Fixed some bugs

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,51 +1,48 @@
-import React, {Component} from 'react';
-import Navbar from "./Navbar";
-import Search from './Search';
-import Chats from './Chats';
-import * as messageActions from "../actions/message";
 import {connect} from "react-redux";
+import * as messageActions from "../actions/message";
+import {useEffect} from "react";
+import Chats from "./Chats";
+import Navbar from "./Navbar";
+import Search from "./Search";
 
-class Sidebar extends Component {
+const Sidebar = ({ isAuthenticated, user, getUserChats, chats }) => {
 
-  waitForDetails() {
-    let isAuthenticated = this.props.isAuthenticated
-    let currentUser = {...this.props.user}
-    let getUserChats = this.props.getUserChats
-    const component = this;
-    console.log(this.props.access)
-    setTimeout(function() {
-      if (
-        isAuthenticated
-      ) {
-        console.log(currentUser)
-        getUserChats(
-          currentUser.id,
+  const waitForDetails = () => {
+    let currentUser = {...user}
+
+    setTimeout(function () {
+      if(isAuthenticated && currentUser.id) {
+         getUserChats(
+          currentUser.id
         );
         return;
       } else {
-        console.log("waiting for details...");
-        component.waitForDetails()
+        console.log("Waiting for details")
+        waitForDetails()
       }
     }, 1000);
   }
 
-  componentDidMount() {
-    this.waitForDetails();
-  }
+  useEffect(() => {
+    setTimeout(() => {
+      waitForDetails();
+    }, 100)
 
-  render() {
-    let activeChats = this.props.chats.map(c => {
-      return (
-        <Chats
-          key={c.id}
-          contact={c.participants[0]}
-          picURL="http://emilcarlsson.se/assets/louislitt.png"
-          status="busy"
-          chatURL={`/${c.id}`}
-        />
-      );
-    });
+  }, [])
+
+  let activeChats = chats.map(c => {
     return (
+      <Chats
+        key={c.id}
+        contact={c.participants[0]}
+        picURL="http://emilcarlsson.se/assets/louislitt.png"
+        status="busy"
+        chatURL={`/${c.id}`}
+      />
+    );
+  });
+
+return(
     <div className={"sidebar"}>
       <Navbar />
       <Search />
@@ -53,15 +50,14 @@ class Sidebar extends Component {
         {activeChats}
       </ul>
     </div>
-    );
-  }
+  );
 }
 
 const mapStateToProps = state => ({
-    isAuthenticated: state.auth.isAuthenticated,
-    token: state.auth.access,
-    user: state.auth.user,
-    chats: state.message.chats
+  isAuthenticated: state.auth.isAuthenticated,
+  token: state.auth.access,
+  user: state.auth.user,
+  chats: state.message.chats
 });
 
 const mapDispatchToProps = dispatch => {
