5135cd8b2f4486eb6c0b47f0897a1c2e21fd6fb5
Dawid Zareba <dawid.zareba03@gmail.com>
2022-12-04 21:42:36 +0100

    Nothing special

--- frontend/src/settings.js
+++ frontend/src/settings.js
@@ -1,4 +1,4 @@
-const  API_URL = "http://127.0.0.1:8000";
-const  SOCKET_URL = "ws://127.0.0.1:8000";
+const API_URL = "http://127.0.0.1:8000";
+const SOCKET_URL = "ws://127.0.0.1:8000";
 
 export { API_URL, SOCKET_URL };
