880a5f5a73f134914f24a08e3357d3b255cc0388
Patryk Wąsowski <77150826+Enchxx@users.noreply.github.com>
2022-12-08 16:26:26 +0100

    Dropdown menu (#24)

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,7 +2,11 @@ import React, {useState} from 'react';
 import { logout } from '../actions/auth'
 import {connect} from "react-redux";
 import {Navigate} from "react-router-dom";
-import {Button} from "react-bootstrap";
+import { Button , Dropdown} from "react-bootstrap";
+import { ReactComponent as ChevronIcon } from '../assets/img/chevron.svg';
+import { ReactComponent as CogIcon } from '../assets/img/cog.svg';
+import { ReactComponent as LogoutIcon } from '../assets/img/logout.svg';
+import { ReactComponent as CaretIcon } from '../assets/img/caret.svg';
 
 const Navbar = ({ logout, user }) => {
   const [redirect, setRedirect] = useState(false);
@@ -21,7 +25,34 @@ const Navbar = ({ logout, user }) => {
       <div className={"user"}>
         <img className={"img"} src={currentUser.avatar} alt={""}/>
         <span className={"name"}>{currentUser.first_name} {currentUser.last_name}</span>
-        <Button className={'btn'} onClick={() => handleLogout()}>Wyloguj</Button>
+
+
+        <Dropdown>
+          <Dropdown.Toggle
+              className={'dropdown-button'}
+          >
+            {/*<CaretIcon/>*/}
+            <ChevronIcon className={'chevron'}/>
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu
+              className={'dropdown-menu'}
+          >
+            <Dropdown.Item
+                className={'dropdown-item'}
+            >
+              <CogIcon className={'dropdown-img'}/>
+              <p>Ustawienia</p>
+            </Dropdown.Item>
+            <Dropdown.Item
+                className={'dropdown-item'}
+            >
+              <LogoutIcon className={'dropdown-img'}/>
+              <p className={''} onClick={() => handleLogout()}>Wyloguj</p>
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+
       </div>
       {redirect ? <Navigate to='/login' /> : null}
     </div>
