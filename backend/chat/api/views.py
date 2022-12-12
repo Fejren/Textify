e3e411804c249fac3fcd251ce7bccdf70e0ebcab
Dawid Zareba <dawid.zareba03@gmail.com>
2022-12-12 15:43:26 +0100

    Deleted contact

--- backend/chat/api/views.py
+++ backend/chat/api/views.py
@@ -1,10 +1,12 @@
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, mixins
 from core.models import Chat
 from .serializers import ChatSerializer
 
 from rest_framework import permissions
 
-from ..views import get_user_contact, get_contact
+User = get_user_model()
 
 
 class ChatViewSet(viewsets.GenericViewSet,
@@ -27,14 +29,14 @@ class ChatViewSet(viewsets.GenericViewSet,
             queryset = Chat.objects.all()
             user_id = self.request.query_params.get('id', None)
             if user_id is not None:
-                contact = get_user_contact(user_id)
-                queryset = contact.chats.all()
+                user = get_object_or_404(User, id=user_id)
+                queryset = user.chats.all()
             return queryset
 
     def perform_create(self, serializer):
         participants = self.request.data.get("participants", None)
-        contacts = []
+        users = []
         for email in participants:
-            contact = get_contact(email)
-            contacts.append(contact)
-        serializer.save(participants=contacts, messages=[])
+            user = get_object_or_404(User, email=email)
+            users.append(user)
+        serializer.save(participants=users, messages=[])
